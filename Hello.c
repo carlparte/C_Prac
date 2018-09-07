ac07642f5600282b32d7e1c5228157703de6e80e
carlparte <carlparte@yahoo.com>
2018-09-07 18:58:29 +0800

    09-07 function and data type(char)

--- Hello.c
+++ Hello.c
@@ -1,6 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+int addTwo(int a, int b) //function: add two number together
+{
+  return a + b;
+}
+char pri(int n) //test char and ASCII
+{
+  return 'A' + n;
+}
 int main()
 {
   printf("hello, world!\n");
+  printf("%d\n", addTwo(2, 3));
+  printf("%c\n", pri(1));
+
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%d", i);
+  }
+  printf("\n");
   return 0;
 }
