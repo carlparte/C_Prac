fd378339ef8392b62ad29c06e16e8d590b71068c
carlparte <carlparte@yahoo.com>
2018-09-29 17:20:51 +0800

    rearrange

--- Lpointer.c
+++ Lpointer.c
@@ -1,7 +1,19 @@
 //指针
 #include <stdio.h>
-void swap(char *v1, char *v2);
-int sumAndMinus(int v1,int v2,int *minus);
+
+void swap(char *v1, char *v2)
+{
+  char temp;
+  temp=*v1;
+  *v1=*v2;
+  *v2=temp; //体会指针的作用，直接修改内存地址的指向内容
+}
+int sumAndMinus(int v1,int v2,int *minus)
+{
+  *minus=v1-v2;
+  return v1+v2; //用指针实现函数的多个返回结果。不通过函数，而是直接修改内存。
+}
+
 int main()
 {
   // int a = 10;
@@ -35,16 +47,4 @@ int main()
   } //指针遍历字符串
 
   return 0;
-}
-void swap(char *v1, char *v2)
-{
-  char temp;
-  temp=*v1;
-  *v1=*v2;
-  *v2=temp; //体会指针的作用，直接修改内存地址的指向内容
-}
-int sumAndMinus(int v1,int v2,int *minus)
-{
-  *minus=v1-v2;
-  return v1+v2; //用指针实现函数的多个返回结果。不通过函数，而是直接修改内存。
 }
