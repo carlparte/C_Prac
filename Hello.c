e2cc4121f78c740ff4f105637d4eff249272adb8
carlparte <carlparte@yahoo.com>
2019-05-12 21:59:26 +0800

    new plat

--- Hello.c
+++ Hello.c
@@ -16,7 +16,7 @@ int main()
   printf("hello, world!\n");
   printf("%d\n", addTwo(2, 3));
   printf("%c\n", pri(1));
-  printf("%d\n", camp(1, 2));
+  printf("%d\n", camp(1, 3));
 
   for (int i = 0; i < 10; i++)
   {
